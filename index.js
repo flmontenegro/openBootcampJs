ba3c8f73eab9e7ed48bfcb4ce232dea3f4d5d2c9
Florencia Montenegro <mflortrabajos@gmail.com>
2022-08-17 20:22:54 -0300

    entrega II

---
+++ index.js
@@ -0,0 +1,13 @@
+const presentacion = {
+    nombre: "Florencia",
+    edad: 26,
+    desarrolladora: false,
+    nacimiento: new Date(1996, 0, 14),
+    libroFav: {
+        titulo: "El Aleph",
+        autor: "Jorge Luis Borges",
+        fecha: new Date(1949),
+        url: "https://bcn.gob.ar/uploads/posts/El-Aleph.pdf"
+    }
+}
+console.log(presentacion);
